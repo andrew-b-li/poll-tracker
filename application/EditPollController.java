56b30aeba2ceb0bf335aa2489eb1534ceca4c54a
dvij99 <60762096+dvij99@users.noreply.github.com>
2020-03-12 16:23:04 -0600

    Working copy of EditPoll. Will add comments later

--- application/EditPollController.java
+++ application/EditPollController.java
@@ -3,6 +3,7 @@ package application;
 import model.Factory;
 import model.Poll;
 import model.PollList;
+import model.Party;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -25,44 +26,71 @@ public class EditPollController extends PollTrackerController{
     private ChoiceBox<String> partyEditName;
     
     private PollList localPolls;
-    private Factory localFactory = new Factory(325);
+    private Factory localFactory;
+    private int localNumberOfSeats;
+    private float localPercentageOfVotes;
     
-	
-    @FXML
-    void initialize() {
-    	String[] partyList = {"Green", "Blue", "Red"};
-    	localFactory.setPartyNames(partyList);
-    	localPolls = localFactory.createRandomPollList(3);
-    	/*Poll[] polls = localPolls.getPolls();
-    	String[] pollNames = new String[polls.length];
-    	for(int i= 0; i < pollNames.length; i++) {
-    		pollNames[i] = polls[i].getPollName();
-    	}
-    	pollEditName.setItems(FXCollections.observableArrayList(pollNames));*/
-   
+    public void setPollTrackerApp(PollTrackerApp app) {
+    	this.app = app;
+    	refresh();
     }
     
     @FXML
     void handleClear(ActionEvent event) {
-    	/*numOfSeats.clear();
-    	projectedPercentageOfVotes.clear();
-    	pollEditName.setValue(null);
-    	partyEditName.setValue(null);*/
-    	
+    	refresh();
     }
 
     @FXML
     void handleUpdate(ActionEvent event) {
+    	if (numOfSeats.getText() == null) {
+    		System.out.println("Need number of seats");
+    	}
+    	else {
+    		localNumberOfSeats = Integer.parseInt(numOfSeats.getText());
+    	}
+    	if (projectedPercentageOfVotes.getText() == null) {
+    		System.out.println("Need number of seats");
+    	}
+    	else {
+    		localPercentageOfVotes = Float.parseFloat(projectedPercentageOfVotes.getText());
+    	}
+    	String nameOfSelectedPoll = pollEditName.getSelectionModel().getSelectedItem();
+    	String nameOfSelectedParty = partyEditName.getSelectionModel().getSelectedItem();
+    	this.localFactory = app.getFactory();
+    	this.localPolls = app.getPolls();
+    	Poll[] polls = localPolls.getPolls();
+    	for (int i = 0; i < polls.length; i++) {
+    		if (polls[i].getPollName().contentEquals(nameOfSelectedPoll)) {
+    			Poll selectedPoll = polls[i];
+    			String[] partyNames = localFactory.getPartyNames();
+    			for (int j = 0; j < selectedPoll.getNumberOfParties(); j++) {
+    				if (partyNames[j].contentEquals(nameOfSelectedParty)) {
+    					Party partyEdited = new Party(nameOfSelectedParty, (float)localNumberOfSeats, localPercentageOfVotes/100);
+    					selectedPoll.addParty(partyEdited);
+    				}
+    			}
+    		}
+    	}
+    	app.setFactory(localFactory);
+    	app.setPolls(localPolls);
     	
     }
     
     public void refresh() {
-    	/*localPolls = super.getPolls();
-    	localFactory = super.getFactory();
+    	this.localFactory = app.getFactory();
+    	this.localPolls = app.getPolls();
+    	Poll[] polls = localPolls.getPolls();
+    	String[] pollNames = new String[polls.length];
+    	for(int i= 0; i < pollNames.length; i++) {
+    		pollNames[i] = polls[i].getPollName();
+    	}
+    	pollEditName.setItems(FXCollections.observableArrayList(pollNames));
+    	
+    	String[] partyNames = localFactory.getPartyNames();
+    	partyEditName.setItems(FXCollections.observableArrayList(partyNames));
+
     	numOfSeats.clear();
     	projectedPercentageOfVotes.clear();
-    	pollEditName.setValue(null);
-    	partyEditName.setValue(null);*/
     }
     
 }
