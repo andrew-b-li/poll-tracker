930509e86cbac996bb3d1af0137e299b28c1be1c
Andrew <Andrew@192.168.1.71>
2020-03-17 23:59:11 -0600

    working AddPollController

--- application/AddPollController.java
+++ application/AddPollController.java
@@ -3,108 +3,72 @@ package application;
 import java.util.ArrayList;
 
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
+
 import model.Factory;
+import model.Party;
 import model.Poll;
 import model.PollList;
 
 public class AddPollController extends PollTrackerController {
-    private PollTrackerApp app;
-    private Factory factory;
+
+    
     @FXML
     private TextField pollName;
     @FXML
-    private ChoiceBox < String > enterPollPosition;
+    private ChoiceBox<String> enterPollPosition;
+    
+	private PollList localPollList;
+    private Factory localFactory;
+    private PollList tempPollList;
 
-    private PollList localPollList;
-
-    ObservableList < String > pollnames = FXCollections.observableArrayList();
-
-    public void setPollTrackerApp(PollTrackerApp app) {
-        this.app = app;
-        this.factory = app.getFactory();
+    public void handleClear(ActionEvent event) {
         refresh();
     }
 
-    public void handleClear() {
-        refresh();
-    }
-
-    public void handleSubmit() {
-        String pollToReplace = enterPollPosition.getValue();
-        int pollCount = 0;
-        for (Poll aPoll: localPollList.getPolls()) {
-            pollCount = pollCount + 1;
-        }
-        PollList tempLocalPollList = new PollList(pollCount, localPollList.getNumOfSeats());
-
-        if (pollToReplace == null) {
+    public void handleSubmit(ActionEvent event) {
+    	if (pollName.getText().equals(null)) {
+    		System.out.println("Need a name");
+    	}
+    	int pollToReplace = enterPollPosition.getSelectionModel().getSelectedIndex();
+    	System.out.println(pollToReplace);
+        
+    	if (pollToReplace == -1) {
             System.out.println("Please Choose Something");
-        } else {
-            for (Poll aPoll: localPollList.getPolls()) {
-                if (pollToReplace == aPoll.getPollName()) {
-                    Poll newPoll = factory.createRandomPoll(pollName.getText());
-                    tempLocalPollList.addPoll(newPoll);
-                } else {
-                    tempLocalPollList.addPoll(aPoll);
-                }
-            }
+        } 
 
-            localPollList = tempLocalPollList;
-    		app.setPolls(localPollList);
+        Poll[] polls = localPollList.getPolls();
+        int numOfPolls = polls.length;
+        int numOfSeats = localPollList.getNumOfSeats();
+        tempPollList = new PollList(numOfPolls, numOfSeats);
+        for (int i = 0; i < numOfPolls; i++) {
+        	if (pollToReplace == i) {
+        		Poll newPoll = localFactory.createRandomPoll(pollName.getText());
+        		tempPollList.addPoll(newPoll);
+        	}
+        	else {
+        		tempPollList.addPoll(polls[i]);
+        	}
+        			
         }
-        refresh();
+        setPolls(tempPollList);
+    	setFactory(localFactory);
+    	refresh();
     }
-
-    //    private PollList generatePollList(int chosenNumOfPolls, int chosenNumOfSeats, String[] chosenNameList) {
-    //        PollList newPollList = new PollList(chosenNumOfPolls, chosenNumOfSeats);
-    //        Factory factory = new Factory(chosenNumOfSeats);
-    //        factory.setPartyNames(chosenNameList);
-    //        for (int i = 1; i <= chosenNumOfPolls; i++) {
-    //            newPollList.addPoll(factory.createRandomPoll("Poll #" + i));
-    //        }
-    //        return newPollList;
-    //    }
-
 
     @Override
     public void refresh() {
-
-
-        localPollList = this.app.getPolls();
-
-        int numberOfSeats = localPollList.getNumOfSeats();
-        int numberOfPolls = 0;
-        String[] partyNames = factory.getPartyNames();
-
-        if (pollnames.size() == 0) {
-
-            for (Poll aPoll: localPollList.getPolls()) {
-                pollnames.add(aPoll.getPollName());
-            }
-
-            if (pollnames.size() > 0) {
-                enterPollPosition.setItems(pollnames);
-            }
-
-        } else {
-            pollnames.clear();
-            System.out.println("clear");
-            for (Poll aPoll: localPollList.getPolls()) {
-                pollnames.add(aPoll.getPollName());
-            }
-            if (pollnames.size() > 0) {
-                enterPollPosition.setItems(pollnames);
-            }
-        }
-        numberOfPolls = pollnames.size();
-        
-        for (String name : partyNames) {
-        System.out.println(name);
-        }
-
+        localPollList = getPolls();
+        localFactory = getFactory();
+    	Poll[] polls = localPollList.getPolls();
+    	String[] pollNames = new String[polls.length];
+    	for(int i= 0; i < pollNames.length; i++) {
+    		pollNames[i] = i+1 + "(replaces poll " + polls[i].getPollName() + ")";
+    	}
+    	enterPollPosition.setItems(FXCollections.observableArrayList(pollNames));
+        pollName.clear();
     }
 }
