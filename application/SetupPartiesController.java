94fbaa3edfa029c17b4f2e4d4f37021fbda05968
Olivia Brash <olivia.brash@gmail.com>
2020-03-30 21:49:30 -0600

    "added Kylie's branch, fixed fxml file"

---
+++ application/SetupPartiesController.java
@@ -0,0 +1,42 @@
+package application;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.*;
+import model.Factory;
+
+public class SetupPartiesController extends PollTrackerController {
+	private PollTrackerApp app;
+	
+	private ChoiceBox<String> partyMenu;
+	private ObservableList<String> partyNames;
+	private TextField newPartyName;
+	
+	
+	private Factory localFactory;
+	
+	public void setPollTrackerApp(PollTrackerApp app) {
+		this.app = app;
+	}
+	
+	public void handleSetPartyInfo () {
+		int selectedIndex = partyMenu.getSelectionModel().getSelectedIndex();
+		String newName = newPartyName.getText();
+		partyMenu.getItems().remove(selectedIndex);
+		partyMenu.getItems().add(selectedIndex, newName);
+		partyNames.set(selectedIndex, newPartyName.getText());
+	}
+	
+	public void handleSubmitPartyInfo () {
+		app.getFactory().setPartyNames(partyNames.toArray(new String[0]));
+		refresh();
+	}
+	
+	public void handleClear() {
+		refresh();
+	}
+	
+	public void refresh() {
+		partyMenu.setItems(FXCollections.observableArrayList(localFactory.getPartyNames()));
+	}
+}
